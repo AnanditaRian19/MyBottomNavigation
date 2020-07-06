a390915b81d51e29e2508a0bb6a0611c6a667db0
AnanditaRian19 <rizkianandita6@gmail.com>
2020-07-06 09:42:34 +0700

    First commit bottom navigation menu

---
+++ app/src/main/java/com/belajar/mybottomnavigation/ui/profile/ProfileFragment.java
@@ -0,0 +1,41 @@
+package com.belajar.mybottomnavigation.ui.profile;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.belajar.mybottomnavigation.R;
+import com.belajar.mybottomnavigation.ui.dashboard.DashboardViewModel;
+
+public class ProfileFragment extends Fragment {
+
+    private ProfileViewModel profileViewModel;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        profileViewModel = ViewModelProviders.of(this).get(ProfileViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+        final TextView textView = root.findViewById(R.id.text_profile);
+        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+
+//        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
